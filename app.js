c55d6686d68d830f1cb1094cb8211f61b2df60f4
Rodrigo <digo.s.oliv@gmail.com>
2022-09-02 19:23:00 -0300

    Criação css e início função forteia número

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+function sorteiaNumero(){
+
+    let valor = document.querySelector('#valor').value
+
+    let sorteio = Math.round(Math.random() *10);
+}
+
+let sorteio = Math.round(Math.random() *10);
+
+console.log(sorteio)
