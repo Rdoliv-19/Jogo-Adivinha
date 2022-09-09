03d827a3cdfa4430fca9637b21c00108a8b293af
Rodrigo <digo.s.oliv@gmail.com>
2022-09-09 16:44:46 -0300

    adição de tentativas jogo adivinha

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
+let tentativas = 10
+
 function sorteiaNumero(){
 
     let valor = parseInt(document.querySelector('#valor').value);
     let resultado = document.querySelector('#resultado');
-
+    let tentativasResultado = document.querySelector('#tentativas-resultado');
 
     let sorteio = parseInt(Math.round(Math.random() *10));
 
+    if (tentativas > 0){
 
     if (valor === sorteio){
-        resultado.innerHTML=("Acertou");
-        
-    } else {
-        resultado.innerHTML=("Errou");
-    } 
+        resultado.innerHTML=("Acertou");        
+    } else if(valor >10 || valor <0){
+        alert('Digite um número de 0 a 10')
+    } else if (sorteio > valor ) {
+        resultado.innerHTML=("Você errou o número sorteado foi MAIOR que seu chute");
+        tentativasResultado.innerHTML=(`Você tem mais ${tentativas} tentativas`)
+    } else if ( sorteio < valor ){
+        resultado.innerHTML=("Você errou o número sorteado foi MENOR que seu chute");
+        tentativasResultado.innerHTML=(`Você tem mais ${tentativas} tentativas`)
+    }
+
+    tentativas = tentativas -1;
+    console.log(tentativas)
+    } else{
+        alert("Acabaram suas tentativas recarregua e tente novamente")
+    }
 }
